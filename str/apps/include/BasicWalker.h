cba0e46085769d23438989190d83f64d33ff12a8
Futa Hirakoba <korosuke613613@gmail.com>
2018-08-27 13:40:45 +0900

    fix BasicWalker

--- str/apps/include/BasicWalker.h
+++ str/apps/include/BasicWalker.h
@@ -1,29 +1,28 @@
 #ifndef __BASIC_WALKER__
 #define __BASIC_WALKER__
 
-#include "Clock.h"
-#include "ColorSensor.h"
+#include "Controller.h"
 #include "SpeedControl.h"
 #include "Walker.h"
-#include "util.h"
-
-using namespace ev3api;
 
 class BasicWalker {
  public:
-  BasicWalker();
+  explicit BasicWalker(Controller& controller_) : controller(controller_)
+  {
+    controller.speakerSetVolume(100);
+  }
   void reset(void);
   void parkingLeft(void);
   void setPidWithoutTarget(double, double, double);
   void spin(bool, int32_t);
-  void goStraight(int32_t, int32_t, int16_t, ColorSensor colorSensor);
-  void goStraight_b(int32_t, int32_t, int16_t, ColorSensor colorSensor);
+  void goStraight(int32_t, int32_t, int16_t);
+  void goStraight_b(int32_t, int32_t, int16_t);
   void backStraight(int32_t, int32_t);
   const bool SPIN_LEFT = false;
   const bool SPIN_RIGHT = true;
 
  private:
-  Clock clock;
+  Controller controller;
   Walker walker;
   SpeedControl speedControl;
   int32_t forward;
