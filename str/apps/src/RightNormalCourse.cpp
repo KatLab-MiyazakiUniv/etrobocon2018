47f81ab06f40a77a542b93b68426357c0d4cb558
Takahiro <tkhr.corei7@gmail.com>
2018-09-25 22:19:23 +0900

    STRAIGHT_LONGを限界感度法？を使用してPID再調整中(やり方が正しいのかはわからない)

--- str/apps/src/RightNormalCourse.cpp
+++ str/apps/src/RightNormalCourse.cpp
@@ -21,12 +21,12 @@ bool RightNormalCourse::runNormalCourse(int16_t target_brightness)
 {
   switch(status) {
     case RightStatus::STRAIGHT_LONG:
-      lineTracerWalker.speedControl.setPid(5.0, 1.0, 0.1, 180.0);
-      lineTracerWalker.turnControl.setPid(1.9, 0.1, 0.1, target_brightness);
+      lineTracerWalker.speedControl.setPid(4.5, 1.0, 0.1, 180.0);
+      lineTracerWalker.turnControl.setPid(0.49999, 0.0, 0.0, target_brightness);
       break;
 
     case RightStatus::STRAIGHT_SHORT:
-      lineTracerWalker.speedControl.setPid(10.0, 1.0, 0.1, 180.0);
+      lineTracerWalker.speedControl.setPid(2.4, 1.0, 0.1, 180.0);
       lineTracerWalker.turnControl.setPid(2.0, 0.1, 0.1, target_brightness);
       break;
 
@@ -40,8 +40,13 @@ bool RightNormalCourse::runNormalCourse(int16_t target_brightness)
       lineTracerWalker.turnControl.setPid(2.0, 0.1, 0.1, target_brightness);
       break;
 
-    case RightStatus::SLOW_DOWN:
-      lineTracerWalker.speedControl.setPid(5.0, 1.0, 0.1, 80.0);
+    case RightStatus::CURVE_OUTSIDE:
+      lineTracerWalker.speedControl.setPid(2.5, 0.01, 0.12, 150.0);
+      lineTracerWalker.turnControl.setPid(2.0, 0.1, 0.1, target_brightness);
+      break;
+
+    case RightStatus::SLOW:
+      lineTracerWalker.speedControl.setPid(2.5, 0.01, 0.12, 150.0);
       lineTracerWalker.turnControl.setPid(2.0, 0.1, 0.1, target_brightness);
       break;
 
@@ -70,31 +75,33 @@ bool RightNormalCourse::statusCheck(int32_t countL, int32_t countR)
   distanse_total = distance.getDistanceTotal(countL, countR);
   old_status = status;
   
-  /*
-  if(distanse_total < 2500)
+  
+  if(distanse_total < 2500)                 //Part 1
     status = RightStatus::STRAIGHT_LONG;
-  else if(distanse_total < 4495)
+  else if(distanse_total < 4495)            //Part 2
     status = RightStatus::CURVE_INSIDE_LONG;
-  else if(distanse_total < 5056)
+  else if(distanse_total < 5056)            //Part 3
     status = RightStatus::STRAIGHT_SHORT;
-  else if(distanse_total < 5640)
+  else if(distanse_total < 5640)            //Part 4
     status = RightStatus::CURVE_INSIDE_SHORT;
-  else if(distanse_total < 6542)
+  else if(distanse_total < 6542)            //Part 5
     status = RightStatus::CURVE_OUTSIDE;
-  else if(distanse_total < 7180)
-    status = RightStatus::STRAIGHT_SHORT;
-  else if(distanse_total < 8640)
+  else if(distanse_total < 7100)            //Part 6
+    status = RightStatus::SLOW;
+  else if(distanse_total < 8680)            //Part 7
     status = RightStatus::CURVE_OUTSIDE;
-  else if(distanse_total < 11540)
+  else if(distanse_total < 11540)           //Part 8
     status = RightStatus::STRAIGHT_LONG;
-  else if(distanse_total < 12430)//ちょうどいい感じ
+  else if(distanse_total < 12430)           //Part 9, goal to gray line
   //else if(distanse_total < 12630)//灰色を完全に通過
     status = RightStatus::CURVE_INSIDE_SHORT;
-  */
+  
+  /*
    if(distanse_total < 750)
      status = RightStatus::SLOW_DOWN;
    else if(distanse_total < 2250)
      status = RightStatus::SLOW_DOWN;
+  */
   else
     status = RightStatus::STOP;
   if(old_status != status) return true;
