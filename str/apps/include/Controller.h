1d72a82606539e579eaac341824b38d90851b427
Morichan <nintendo698@gmail.com>
2018-09-28 13:55:17 +0900

    fix: ビルドを通すためにKatLab-MiyazakiUniv/etrobocon2018リポジトリのControllerクラスのヘッダファイルをコピペ

--- str/apps/include/Controller.h
+++ str/apps/include/Controller.h
@@ -1,103 +1,67 @@
 #ifndef __CONTROLLER__
 #define __CONTROLLER__
 
-#include <cstdint>
-#include <cstdlib>
-#include <iostream>
+#include "Clock.h"
+#include "ColorSensor.h"
+#include "Motor.h"
+#include "TouchSensor.h"
+#include "ev3api.h"
+#include <stdarg.h>
 
-class Motor {
- public:
-  double count = 0.0;
-  int getCount() { return static_cast<int>(count); };
-  void setPWM(int pwm)
-  {
-    count += pwm * 0.05;
-    // std::cout << count << std::endl;
-  };
-  void stop(){};
-  void reset(){};
-  void setBrake(bool isBrake){};
-};
+/*
+ * touch_sensor = EV3_PORT_1;
+ * sonar_sensor = EV3_PORT_2;
+ * color_sensor = EV3_PORT_3;
+ * gyro_sensor  = EV3_PORT_4;
+ *
+ * left_motor   = EV3_PORT_C;
+ * right_motor  = EV3_PORT_B;
+ * lift_motor   = EV3_PORT_A;
+ * tail_motor   = EV3_PORT_D;
+ */
 
-class Clock {
- public:
-  void sleep(int time){};
-};
+using namespace ev3api;
 
-class TouchSensor {
- public:
-  bool isPressed() { return false; }
-};
-
-class ColorSensor {
- public:
-  int getBrightness() { return brightness; }
-  int brightness = 0;
-};
+extern void init_f(const char* str);
+extern void msg_f(const char* str, int32_t line);
 
 class Controller {
  public:
-  Clock clock;
-  Motor rightWheel;
-  Motor leftWheel;
-  Motor liftMotor;
+  Controller()
+    : touchSensor(PORT_1),
+      colorSensor(PORT_3),
+      liftMotor(PORT_A),
+      rightWheel(PORT_B),
+      leftWheel(PORT_C),
+      tailMotor(PORT_D)
+  {
+  }
+  uint16_t noteFs6 = NOTE_FS6;
+  uint16_t noteFs4 = NOTE_FS4;
   TouchSensor touchSensor;
   ColorSensor colorSensor;
+  Motor liftMotor;
+  Motor rightWheel;
+  Motor leftWheel;
+  Motor tailMotor;
+  Clock clock;
+  void speakerSetVolume(uint8_t volume);
+  void ledSetColorOrange();
+  void ledSetColorGreen();
+  int16_t getBrightness();
+  void speakerPlayTone(uint16_t frequency, int32_t duration);
+  bool buttonIsPressedBack();
+  bool buttonIsPressedUp();
+  bool buttonIsPressedDown();
+  bool buttonIsPressedRight();
+  bool buttonIsPressedLeft();
+  bool buttonIsPressedEnter();
+  void tslpTsk(int16_t time);  // 4msec周期起動
+  void printDisplay(int8_t row, const char* format, ...);
+  void getRawColor(uint16_t& r, uint16_t& g, uint16_t& b);
 
-  int noteFs6 = 0;
-  int noteFs4 = 0;
-  Controller() { mock_r = mock_g = mock_b = 0; }
-  void speakerSetVolume(int volume){};
-  void ledSetColorOrange(){};
-  void ledSetColorGreen(){};
-  int getBrightness() { return brightness; };
-  void speakerPlayTone(int frequency, int duration){};
-  bool buttonIsPressedBack()
-  {
-    counter++;
-    if(counter >= countLimit) {
-      return true;
-    }
-    return false;
-  };
-  void getRawColor(std::uint16_t& r, std::uint16_t& g, std::uint16_t& b)
-  {
-    r = mock_r;
-    g = mock_g;
-    b = mock_b;
-
-    return;
-  };
-  bool buttonIsPressedUp() { return false; };
-  bool buttonIsPressedDown() { return false; };
-  bool buttonIsPressedRight() { return false; };
-  bool buttonIsPressedLeft() { return false; };
-  bool buttonIsPressedEnter()
-  {
-    static int counter = 0;
-    counter++;
-    if(counter >= 10) {
-      return true;
-    }
-    return false;
-  };
-  void tslpTsk(int time)
-  {
-    exitCounter++;
-    if(exitCounter > exitCountLimit) std::exit(1);
-  };  // 4msec周期起動
-  void printDisplay(int row, const char* format, ...){};
-  int countLimit = 100;
-  int counter = 0;
-  int exitCounter = 0;
-  int exitCountLimit = 1000;
-  int brightness = 0;
-  std::uint16_t mock_r, mock_g, mock_b;
-  void setMockRgb(std::uint16_t r, std::uint16_t g, std::uint16_t b)
-  {
-    mock_r = r;
-    mock_g = g;
-    mock_b = b;
-  }
+ private:
+  rgb_raw_t rgb;
+  char msg[32];
 };
 #endif
