3638fd90184943ea23b8b3498ed5905d7e75c766
T.Miyaji <thomath0901@gmail.com>
2018-10-03 18:31:08 +0900

    refactor: Pidクラスの委譲コンストラクタを作成 & PID制御パラメータをまとめたリテラルクラスを作成

--- str/apps/include/Pid.h
+++ str/apps/include/Pid.h
@@ -1,10 +1,36 @@
 /**
  * @file Pid.h
  * @brief PID制御の計算を行うクラス
- * @author Futa HIRAKOBA
+ * @author Futa HIRAKOBA, T.Miyaji
  */
-#ifndef __PID__
-#define __PID__
+#ifndef PID_H
+#define PID_H
+
+/*!
+ *  @class PidParameter
+ *  @brief  PID制御で用いるパラメータをまとめたリテラルクラス
+ *  @param  p_gain_ Kp
+ *  @param  i_gain_ Ki
+ *  @param  d_gain_ Kd
+ *  @param  del_    1周期
+ */
+struct PidParameter {
+  double p_gain;
+  double i_gain;
+  double d_gain;
+  double del;
+  constexpr PidParameter(double p_gain_ = 0.0, double i_gain_ = 0.0, double d_gain_ = 0.0,
+                         double del_ = 0.004)
+    : p_gain(p_gain_), i_gain(i_gain_), d_gain(d_gain_), del(del_)
+  {
+  }
+  void set(double p_gain_, double i_gain_, double d_gain_)
+  {
+    p_gain = p_gain_;
+    i_gain = i_gain_;
+    d_gain = d_gain_;
+  }
+};
 
 /*! @class Pid Pid.h "Pid.h"
  *  @brief PID制御の計算を行うクラス
@@ -12,16 +38,16 @@
 class Pid {
  public:
   /** コンストラクタ */
-  Pid(double p_gain, double i_gain, double d_gain, double target);
+  Pid(double p_gain_, double i_gain_, double d_gain_, double target_);
+  Pid(double p_gain_, double i_gain_, double d_gain_, double diff_, double integral_,
+      double target_, double output_);
 
   /** Kp, Ki, Kd, 目標値を設定する
    * @param _p_gain Kp
    * @param _i_gain Ki
    * @param _d_gain Kd
    * @param _target 目標値 */
-  void setPid(double _p_gain, double _i_gain, double _d_gain, double _target);
-  void wrapper_of_constructor(double _p_gain, double _i_gain, double _d_gain, double _diff,
-                              double _integral, double _target, double _output);
+  void setPid(double p_gain_, double i_gain_, double d_gain_, double target_);
 
  protected:
   /** PID制御の計算をする
@@ -38,14 +64,11 @@ class Pid {
   double limitOutput(double pid_value);
 
  private:
-  double p_gain;
-  double i_gain;
-  double d_gain;
+  PidParameter param;
   double old_diff;
   double integral;
   double target;
   double output;
-  double del;
 };
 
 #endif
