fce4b81a3d44552586761e5da7568b7cae18d551
T.Miyaji <thomath0901@gmail.com>
2018-11-05 20:30:56 +0900

    refactor: AIAnswerArrayのコード整形をかけた

--- str/apps/include/AIAnswerArray.h
+++ str/apps/include/AIAnswerArray.h
@@ -6,10 +6,10 @@
 
 #ifndef AI_ANSWER_ARRAY_H
 #define AI_ANSWER_ARRAY_H
-#include <cstdint>
-#include <array>
-#include <initializer_list>
 #include <algorithm>
+#include <array>
+#include <cstdint>
+#include <initializer_list>
 
 namespace AI_Answer {
   template <typename T, std::uint8_t N = 6>
@@ -17,9 +17,10 @@ namespace AI_Answer {
    private:
     T sequence[N];
     std::uint8_t pos;
+
    public:
     std::array<float, 8> probs;
-    constexpr array() : sequence{}, pos(0), probs() { }
+    constexpr array() : sequence{}, pos(0), probs() {}
     //! 配列のサイズを返す
     constexpr std::uint8_t size() noexcept { return N; }
 
@@ -36,48 +37,41 @@ namespace AI_Answer {
     //! 配列の要素を入れる
     void push_back(const T& value) noexcept
     {
-      if(pos >= N)  pos = 0;
+      if(pos >= N) pos = 0;
       sequence[pos] = value;
       ++pos;
     }
 
     void push_back(std::initializer_list<T> values) noexcept
     {
-      for(auto value : values)
-        push_back(value);
+      for(auto value : values) push_back(value);
     }
 
-    constexpr T at(const std::uint8_t i) noexcept
-    {
-      return sequence[i];
-    }
+    constexpr T at(const std::uint8_t i) noexcept { return sequence[i]; }
 
-    constexpr T operator[](const std::uint8_t i) noexcept
-    {
-      return sequence[i];
-    }
+    constexpr T operator[](const std::uint8_t i) noexcept { return sequence[i]; }
 
     // 手書き数字の判定
     std::int8_t handwriting() noexcept
     {
-      if(size() != 6)  return -1;
-      if(sequence[0] == cast(0))  addProb(0, 1);
-      if(sequence[0] == cast(1))  addProb(2, 3, 4, 5, 6, 7);
+      if(size() != 6) return -1;
+      if(sequence[0] == cast(0)) addProb(0, 1);
+      if(sequence[0] == cast(1)) addProb(2, 3, 4, 5, 6, 7);
 
-      if(sequence[1] == cast(0))  addProb(0, 1, 2, 3, 7);
-      if(sequence[1] == cast(1))  addProb(4, 5, 6);
+      if(sequence[1] == cast(0)) addProb(0, 1, 2, 3, 7);
+      if(sequence[1] == cast(1)) addProb(4, 5, 6);
 
-      if(sequence[2] == cast(0))  addProb(1, 3, 5, 7);
-      if(sequence[2] == cast(1))  addProb(0, 2, 4, 6);
+      if(sequence[2] == cast(0)) addProb(1, 3, 5, 7);
+      if(sequence[2] == cast(1)) addProb(0, 2, 4, 6);
 
-      if(sequence[3] == cast(0))  addProb(4);
-      if(sequence[3] == cast(1))  addProb(0, 1, 2, 3, 5, 6, 7);
+      if(sequence[3] == cast(0)) addProb(4);
+      if(sequence[3] == cast(1)) addProb(0, 1, 2, 3, 5, 6, 7);
 
-      if(sequence[4] == cast(0))  addProb(1, 2, 7);
-      if(sequence[4] == cast(1))  addProb(0, 3, 4, 5, 6);
+      if(sequence[4] == cast(0)) addProb(1, 2, 7);
+      if(sequence[4] == cast(1)) addProb(0, 3, 4, 5, 6);
 
-      if(sequence[5] == cast(0))  addProb();
-      if(sequence[5] == cast(1))  addProb(1, 2, 3, 4, 5, 6, 7);
+      if(sequence[5] == cast(0)) addProb();
+      if(sequence[5] == cast(1)) addProb(1, 2, 3, 4, 5, 6, 7);
 
       return maxProbabilityNumber();
     }
@@ -85,24 +79,24 @@ namespace AI_Answer {
     // デジタル数字の判定
     std::int8_t digitalNumber() noexcept
     {
-      if(size() != 6)  return -1;
-      if(sequence[0] == cast(0))  addProb(1, 4);
-      if(sequence[0] == cast(1))  addProb(0, 2, 3, 5, 6, 7);
+      if(size() != 6) return -1;
+      if(sequence[0] == cast(0)) addProb(1, 4);
+      if(sequence[0] == cast(1)) addProb(0, 2, 3, 5, 6, 7);
 
-      if(sequence[1] == cast(0))  addProb(0, 1, 7);
-      if(sequence[1] == cast(1))  addProb(2, 3, 4, 5, 6);
+      if(sequence[1] == cast(0)) addProb(0, 1, 7);
+      if(sequence[1] == cast(1)) addProb(2, 3, 4, 5, 6);
 
-      if(sequence[2] == cast(0))  addProb(1, 4, 7);
-      if(sequence[2] == cast(1))  addProb(0, 2, 3, 5, 6);
+      if(sequence[2] == cast(0)) addProb(1, 4, 7);
+      if(sequence[2] == cast(1)) addProb(0, 2, 3, 5, 6);
 
-      if(sequence[3] == cast(0))  addProb(1, 3, 4, 5, 7);
-      if(sequence[3] == cast(1))  addProb(0, 2, 6);
+      if(sequence[3] == cast(0)) addProb(1, 3, 4, 5, 7);
+      if(sequence[3] == cast(1)) addProb(0, 2, 6);
 
-      if(sequence[4] == cast(0))  addProb(5, 6);
-      if(sequence[4] == cast(1))  addProb(0, 1, 2, 3, 4, 7);
+      if(sequence[4] == cast(0)) addProb(5, 6);
+      if(sequence[4] == cast(1)) addProb(0, 1, 2, 3, 4, 7);
 
-      if(sequence[5] == cast(0))  addProb(1, 2, 3, 7);
-      if(sequence[5] == cast(1))  addProb(0, 4, 5, 6);
+      if(sequence[5] == cast(0)) addProb(1, 2, 3, 7);
+      if(sequence[5] == cast(1)) addProb(0, 4, 5, 6);
 
       return maxProbabilityNumber();
     }
@@ -113,13 +107,9 @@ namespace AI_Answer {
     template <class... Args>
     void addProb(const Args&... args)
     {
-      for(int i : std::initializer_list<int>{args...})
-        probs[i] += 1.0 / size();
+      for(int i : std::initializer_list<int>{ args... }) probs[i] += 1.0 / size();
     }
-
   };
-}
-
-
+}  // namespace AI_Answer
 
 #endif
