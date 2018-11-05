abc071b45fb36ec97a1315dd0f07a1681a5fd6df
T.Miyaji <thomath0901@gmail.com>
2018-11-05 20:09:09 +0900

    feat: Navigatorクラスにbackメソッドを追加した

--- str/apps/include/AIAnswer.h
+++ str/apps/include/AIAnswer.h
@@ -7,15 +7,22 @@
 #define AI_ANSWER_H
 #include "AIAnswerArray.h"
 #include "Navigator.h"
+#include <array>
+#include <cstdint>
 
 class AIAnswer {
  private:
   AI_Answer::array<int, 6> handwriting;
   AI_Answer::array<int, 6> digital;
   Navigator navigator;
+  std::array<std::int16_t, 3> sensor_values;
 
  public:
-  AIAnswer(Controller& controller, Walker& walker) : navigator(controller, walker) {}
+  AIAnswer(Controller& controller, Walker& walker, std::int16_t white, std::int16_t black,
+           std::int16_t brightness)
+    : navigator(controller, walker), sensor_values{ white, black, brightness }
+  {
+  }
   void run();
 };
 
--- str/apps/src/AIAnswer.cpp
+++ str/apps/src/AIAnswer.cpp
@@ -7,13 +7,6 @@
 
 void AIAnswer::run()
 {
-  navigator.move(690.0);
-
-  navigator.spin(90);
-
-  navigator.moveWhileDetecting(360.0);
-
-  navigator.spin(135, true);
-
-  navigator.move(150.0);
+  // 20cm進む
+  navigator.getNearbyBrightness(50);
 }
