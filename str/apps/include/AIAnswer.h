c5bf7c3ce5d8819f7f0bfde3b5a3b85bd054e538
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2018-11-10 17:20:35 +0900

    コンフリクトしていたので頑張って直した

--- str/apps/include/AIAnswer.h
+++ str/apps/include/AIAnswer.h
@@ -1,33 +1,4 @@
 /**
-<<<<<<< HEAD
- *  @file   AIAnswer.h
- *  @brief  AIアンサー
- *  @author T.Miyaji
- */
-#ifndef AI_ANSWER_H
-#define AI_ANSWER_H
-#include "AIAnswerArray.h"
-#include "LineTracerWalker.h"
-#include "Navigator.h"
-#include <array>
-#include <cstdint>
-
-class AIAnswer {
- private:
-  AI_Answer::array<int, 6> handwriting;
-  AI_Answer::array<int, 6> digital;
-  Navigator navigator;
-  std::array<std::int16_t, 3> sensor_values;
-  Walker walker;
-  LineTracerWalker line_tracer;
-  Controller controller;
-  MotorAngle motor_angle;
-
- public:
-  void run();
-  // void goLineTrace(float distance, std::int16_t target_brightness, std::int8_t pwm = 20);
-};
-=======
  * @file AIAnswer.h
  * @brief AIAnswerクラス
  * @author Tatsumi Nishida
@@ -54,6 +25,5 @@ class AIAnswer {
   // 数字に対応したブロックを動かす
   void solveAIAnswer(Controller controller, LineTracerWalker line_tracer, MotorAngle motor_angle);
 }
->>>>>>> 751b48e58956ffd3ea049220ab92d433957e4fe9
 
 #endif
--- str/apps/src/AIAnswer.cpp
+++ str/apps/src/AIAnswer.cpp
@@ -1,5 +1,4 @@
 /**
-<<<<<<< HEAD
  *  @file   AIAnswer.cpp
  *  @brief  AIアンサー
  *  @author T.Miyaji
@@ -11,40 +10,3 @@ void AIAnswer::run()
   // 20cm進む
   navigator.getNearbyBrightness(50);
 }
-
-// void AIAnswer::goLineTrace(float distance, std::int16_t target_brightness, std::int8_t pwm)
-// {
-//   float measure_distance = 0;
-//   std::int16_t luminance = 0;
-//   line_tracer.speedControl.setPid(2.0, 0.8, 0.1, pwm);
-//   line_tracer.turnControl.setPid(1.1, 0.1, 0.2, target_brightness);
-//   while(distance >= measure_distance) {
-//     luminance = controller.getBrightness();
-//     measure_distance = motor_angle.absoluteAngleMean(walker.get_count_L(), walker.get_count_R());
-//     line_tracer.runLine(walker.get_count_L(), walker.get_count_R(), luminance);
-//     controller.printDisplay(5, "distance: %f", measure_distance);
-//     if(line_tracer.getForward() < 0) {
-//       walker.run(0, 0);
-//     } else {
-//       walker.run(line_tracer.getForward(), line_tracer.getTurn());
-//     }
-//     controller.tslpTsk(4); // 4msec周期
-//   }
-//   walker.reset();
-// }
-=======
-*@file AIAnswer.cpp* @brief AIAnswerクラス* @author Tatsumi NISHIDA* /
-
-#include "AIAnswer.h"
-
-    // AIAnswerの数字を読み取る
-    void AIAnswer::readAIAnswer(Controller controller)
-{
-}
-
-// 数字に対応したブロックを動かす
-void AIAnswer::solveAIAnswer(Controller controller, LineTracerWalker lineTracerWalker,
-                             Distance distance)
-{
-}
->>>>>>> 751b48e58956ffd3ea049220ab92d433957e4fe9
